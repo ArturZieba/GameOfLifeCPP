f85c7e5e7b01e094479575e19c5ae75a43c6e9cc
Siwy <43797135+ArturZieba@users.noreply.github.com>
2024-02-29 23:17:38 +0100

    Add separate classes for GOL main loop and cells
    - Add separate GOL and Cell classes to tidy up code.
    - Verify functions from both declared classes can be called inside int main().
    - Add comments describing thought process choosing specific syntax for earlier/current code.

--- GOL.cpp
+++ GOL.cpp
@@ -1,14 +1,22 @@
 #include "GOL.h"
 #include <iostream> //For cout
 #include <cstdlib> //For rand - theoritically not needed, iostream > istream > ostream > ios > xlocnum already includes cstdlib, but it's still good practice to state includes explicitly
-
+//#include <random> //TEMP: Apparently should be used instead of rand()/srand() from <cstdlib>, check out further
 
 int main()
+{
+	GOL::DrawGrid();
+	Cell::IsAlive();
+}
+
+//TEMP: Draw a grid in the Console
+void GOL::DrawGrid()
 {
 	for (int i = 1; i < 10; i++)
 	{
 		for (int j = 1; j < 10; j++)
 		{
+			//TEMP: std::rand should not be used, look for some implementation from <random>
 			std::cout << (std::rand() % 2) ? "1" : "0";
 
 			if (j == 9)
@@ -17,4 +25,11 @@ int main()
 			}
 		}
 	}
+}
+
+//TEMP: Print a string to the Console, currently to check function call from both GOL and Cell classes
+void Cell::IsAlive()
+{
+	std::cout << "Dead or Alive";
+	std::cout << std::endl;
 }
--- GOL.h
+++ GOL.h
@@ -1,5 +1,18 @@
 #pragma once
+//Declaring in separate header to tidy up the source file and not take up space above int main() for declarations
+//Split classes for Game of Life and Cells to tidy up variables/functions further
 class GOL
 {
+//TEMP: Public because it has to be accessible outside the class (i.e. int main()) and there is no need to make it private here and call it by proxy using other public functions (kind of like Getters and Setters for private variables)
+public:
+	//TEMP: Set as static so that instance of parent class is not needed to call this function
+	static void DrawGrid();
 };
+
+class Cell
+{
+public:
+	static void IsAlive();
+};
+
 
